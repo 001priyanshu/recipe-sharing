cfd7a6e29a52a0315906dbb96b4763cb2a1ffa7f
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-10-22 22:59:04 +0530

    loader-added in signin-and-signup-page

--- client/src/components/Registration/Signin.jsx
+++ client/src/components/Registration/Signin.jsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
+
 
 const Signin = () => {
   const [cookies, setCookie] = useCookies(["access_token"]);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
+    setIsLoading(true);
     event.preventDefault();
 
     try {
@@ -19,43 +24,56 @@ const Signin = () => {
         email,
         password,
       });
-      console.log(result);
       setCookie("access_token", JSON.stringify(result.data.token));
       window.localStorage.setItem("userID", JSON.stringify(result.data.user));
+      alert("Signin Successfull");
+      setIsLoading(false);
       navigate("/");
     } catch (error) {
-      console.error(error);
+      setIsLoading(false);
+      alert("Email or password not matched ");
+     
     }
   };
 
   return (
-    <div className="auth-container container mt-8 w-2/3 mx-auto">
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h2 className="text-2xl font-semibold mb-4 text-center">Login</h2>
-        <div className="form-group mb-4">
-          <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="form-group mb-6">
-          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
-          Login
-        </button>
-      </form>
+    <div>
+      {
+        isLoading ? (
+          <div className="flex justify-center" >
+            <RotatingLines type="Oval" color="#00BFFF" height={100} width={100} />
+          </div >
+        ) : (
+
+          <div className="auth-container container mt-8 w-2/3 mx-auto">
+            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <h2 className="text-2xl font-semibold mb-4 text-center">Login</h2>
+              <div className="form-group mb-4">
+                <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Username:</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="form-group mb-6">
+                <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+                Login
+              </button>
+            </form>
+          </div>
+        )}
     </div>
   );
 };
--- client/src/components/Registration/Signup.jsx
+++ client/src/components/Registration/Signup.jsx
@@ -3,12 +3,16 @@ import axios from "axios";
 import { storage } from "../firebase/firebase.init";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { useNavigate } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
+
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [selectedImage, setSelectedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const navigate = useNavigate();
 
   const handleChange = (event) => {
@@ -17,6 +21,7 @@ const Signup = () => {
   };
 
   const handleSubmit = async (event) => {
+    setIsLoading(true);
     event.preventDefault();
 
     try {
@@ -34,117 +39,128 @@ const Signup = () => {
         avatar: url ? url : null,
       });
       if (res.data.success) {
+        setIsLoading(false);
         navigate("/signin");
         alert("Registration Completed! Now login.");
       }
     } catch (error) {
-      console.log(error.message);
+      setIsLoading(false);
+      alert("Email already exist...");
+      
     }
   };
 
   return (
-    <div className="auth-container container w-2/3 mt-8 mx-auto">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      >
-        <h2 className="text-2xl font-semibold mb-4 text-center">Register</h2>
-        <div className="form-group mb-4">
-          <label
-            htmlFor="name"
-            className="block text-gray-700 text-sm font-bold mb-2"
+    <div>
+      {isLoading ? (
+        <div className="flex justify-center" >
+          <RotatingLines type="Oval" color="#00BFFF" height={100} width={100} />
+        </div >
+      ) : (
+        <div className="auth-container container w-2/3 mt-8 mx-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
           >
-            Name:
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="form-group mb-4">
-          <label
-            htmlFor="email"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Email:
-          </label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="form-group mb-6">
-          <label
-            htmlFor="password"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="relative border border-dashed border-gray-300 p-4 rounded-md">
-          {!selectedImage && (
-            <input
-              type="file"
-              accept="image/*"
-              name="photo"
-              className="absolute top-0 left-0 h-full w-full opacity-0"
-              onChange={handleChange}
-            />
-          )}
-          {selectedImage ? (
-            <div className="flex items-center">
-              <svg
-                className="w-6 h-6 text-green-600"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            <h2 className="text-2xl font-semibold mb-4 text-center">Register</h2>
+            <div className="form-group mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 text-sm font-bold mb-2"
               >
-                <path d="M5 13l4 4L19 7"></path>
-              </svg>
-              <p className="ml-2 text-green-600 font-semibold">
-                Image selected
-              </p>
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
             </div>
-          ) : (
-            <div className="flex items-center justify-center">
-              <svg
-                className="w-6 h-6 text-gray-400"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            <div className="form-group mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 text-sm font-bold mb-2"
               >
-                <path d="M12 4v6m0 0v6m0-6h6m-6 0H6"></path>
-              </svg>
-              <p className="ml-2 text-gray-400">Select Avatar</p>
+                Email:
+              </label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
             </div>
-          )}
+            <div className="form-group mb-6">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="relative border border-dashed border-gray-300 p-4 rounded-md">
+              {!selectedImage && (
+                <input
+                  type="file"
+                  accept="image/*"
+                  name="photo"
+                  className="absolute top-0 left-0 h-full w-full opacity-0"
+                  onChange={handleChange}
+                />
+              )}
+              {selectedImage ? (
+                <div className="flex items-center">
+                  <svg
+                    className="w-6 h-6 text-green-600"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path d="M5 13l4 4L19 7"></path>
+                  </svg>
+                  <p className="ml-2 text-green-600 font-semibold">
+                    Image selected
+                  </p>
+                </div>
+              ) : (
+                <div className="flex items-center justify-center">
+                  <svg
+                    className="w-6 h-6 text-gray-400"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path d="M12 4v6m0 0v6m0-6h6m-6 0H6"></path>
+                  </svg>
+                  <p className="ml-2 text-gray-400">Select Avatar</p>
+                </div>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+            >
+              Register
+            </button>
+          </form>
         </div>
-        <button
-          type="submit"
-          className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-        >
-          Register
-        </button>
-      </form>
+      )}
     </div>
   );
 };
